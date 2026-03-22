12ba3be3329e723acd7547bf58efdf97b941e2ad
Knuto1603 <108632698+Knuto1603@users.noreply.github.com>
2026-03-22 16:01:08 -0500

    test: cambiar IP dev a 160.3.0.1 para verificar que prod no usa environment.development

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  securityUrl: 'https://127.0.0.1:8000/api',
-  coreUrl: 'https://127.0.0.1:8001/api'
+  securityUrl: 'https://160.3.0.1:8000/api',
+  coreUrl: 'https://160.3.0.1:8001/api'
 };

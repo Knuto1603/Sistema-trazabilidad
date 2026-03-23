6c4e5a2fefb866728a6be5bc7513dcd0f9d2cc9b
Knuto1603 <108632698+Knuto1603@users.noreply.github.com>
2026-03-23 14:37:56 -0500

    fix(backend): habilitar AttachFileBundle en prod para security app

--- backend/apps/security/config/bundles.php
+++ backend/apps/security/config/bundles.php
@@ -8,6 +8,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
-    CarlosChininin\AttachFile\AttachFileBundle::class => ['dev' => true, 'test' => true],
+    CarlosChininin\AttachFile\AttachFileBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];

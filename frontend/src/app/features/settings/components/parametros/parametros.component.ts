4c8e3477fdd77579cf04af8b2651f43e82619549
Knuto1603 <108632698+Knuto1603@users.noreply.github.com>
2026-04-03 13:55:16 -0500

    fix(parametros): quitar maxLength(100) del campo name y cambiar a textarea

--- frontend/src/app/features/settings/components/parametros/parametros.component.ts
+++ frontend/src/app/features/settings/components/parametros/parametros.component.ts
@@ -37,7 +37,7 @@ export class ParametrosComponent implements OnInit {
   isAdmin = computed(() => this.authService.hasRole('ROLE_ADMIN'));
 
   form = this.fb.group({
-    name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
+    name: ['', [Validators.required, Validators.minLength(2)]],
     alias: ['', [Validators.maxLength(6)]],
     value: [''],
     parentId: ['']
